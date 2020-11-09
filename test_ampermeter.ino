9fa1ab56e7ebf5d08e73afc7c736b2c4891aeb30
uki44 <44674013+uki44@users.noreply.github.com>
2020-11-09 19:54:18 +0100

    Add files via upload

---
+++ test_ampermeter.ino
@@ -0,0 +1,25 @@
+void setup() {
+  pinMode(2,OUTPUT);
+  pinMode(3,OUTPUT);
+  pinMode(4,OUTPUT);
+  pinMode(5,OUTPUT);
+  digitalWrite(2,HIGH);
+  digitalWrite(3,HIGH);
+  digitalWrite(4,HIGH);
+
+}
+
+void loop() {
+
+if(digitalRead(6)==1){
+  digitalWrite(5,HIGH);
+}
+if(digitalRead(7)==1){
+  digitalWrite(5,LOW);
+}
+ /* digitalWrite(5,HIGH);
+  delay(1000);
+  digitalWrite(5,LOW);
+  delay(1000);*/
+  
+}
