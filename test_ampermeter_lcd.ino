a654638882e553ea3f4c491758f3f654b7bf5ad5
uki44 <44674013+uki44@users.noreply.github.com>
2020-12-20 13:39:31 +0100

    Add files via upload

--- test_ampermeter_lcd.ino
+++ test_ampermeter_lcd.ino
@@ -1,9 +1,8 @@
-#code made by Uroš Tomažič
-
+//code made by Uroš Tomažič
 #include <LiquidCrystal.h>
 const int rs = 13, en = 12, d4 = 11, d5 = 10, d6 = 9, d7 = 8;
 LiquidCrystal lcd(rs, en, d4, d5, d6, d7);
-
+int a0;
 void setup() {
   lcd.begin(16, 2);
   pinMode(2,OUTPUT);
@@ -18,13 +17,7 @@ void setup() {
 }
 
 void loop() {
-  pinMode(2,OUTPUT);
-  pinMode(3,OUTPUT);
-  pinMode(4,OUTPUT);
-  pinMode(5,OUTPUT);
-  digitalWrite(2,HIGH);
-  digitalWrite(3,HIGH);
-  digitalWrite(4,HIGH);
+  a0 = analogRead(A0);
 
 if(digitalRead(7)==1){
   digitalWrite(5,LOW);
